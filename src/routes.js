158f6c9d78da505975f715b9b0bb3893a63f8173
Matheus Duarte <matheusdcarvalho007@gmail.com>
2019-09-08 00:21:19 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const routes = require('express').Router();
+const {User} = require('./app/models')
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Duarte',
+        email: 'matheusd007@gmail.com',
+        password_hash: '123456'
+    });
+
+    return res.json({user});
+});
+
+module.exports = routes;
